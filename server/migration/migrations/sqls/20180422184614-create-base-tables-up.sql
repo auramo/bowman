53d8686442036a79ea641547fedc665f6e0148c6
Perttu Auramo <perttu.auramo@gmail.com>
2018-04-22 22:04:46 +0300

    Create base schema

---
+++ server/migration/migrations/sqls/20180422184614-create-base-tables-up.sql
@@ -0,0 +1,20 @@
+CREATE TABLE payment_type (id BIGSERIAL, description TEXT);
+
+CREATE TABLE payment_group(
+  id BIGSERIAL PRIMARY KEY
+);
+
+CREATE TABLE payment_group_user(
+  user_account_id BIGINT NOT NULL REFERENCES user_account (id),
+  payment_group_id BIGINT NOT NULL REFERENCES payment_group(id),
+  UNIQUE(user_account_id, payment_group_id)
+);
+
+CREATE TABLE payments (
+  id UUID PRIMARY KEY,
+  amount_cents BIGINT,
+  payment_date DATE,
+  description TEXT,
+  user_account_id BIGINT NOT NULL REFERENCES user_account (id),
+  payment_group_id BIGINT NOT NULL REFERENCES payment_group (id)
+);
