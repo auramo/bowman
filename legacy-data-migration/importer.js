73078128e2d04692cc6fe96909a8f6e28fda7514
Perttu Auramo <perttu.auramo@gmail.com>
2018-05-01 21:27:33 +0300

    First version which inserts payments also
    Dates and payment types not yet fully done

--- legacy-data-migration/importer.js
+++ legacy-data-migration/importer.js
@@ -4,12 +4,13 @@ const fs = require("fs")
 const db = require("../server/db/db")
 const Promise = require("bluebird")
 const R = require("ramda")
+const assert = require("assert")
 
 const typesFile = "data-to-import/payment-types.json"
 const payersFile = "data-to-import/payers.json"
-const expensesFile = "data-to-import/expenses.json"
+const paymentsFile = "data-to-import/expenses.json"
 const userMappingFile = "data-to-import/user-mapping.json"
-const importFiles = [typesFile, payersFile, expensesFile, userMappingFile]
+const importFiles = [typesFile, payersFile, paymentsFile, userMappingFile]
 
 const checkFiles = () => {
   for (const importFile of importFiles) {
@@ -31,6 +32,22 @@ const insertPaymentType = async ([paymentType, description]) => {
   return [paymentType, { description, id: result.id }]
 }
 
+const insertPayment = async (payment, paymentTypeId, userId) => {
+  await db.query(
+    `INSERT INTO payment (id, amount_cents, payment_date, description, user_account_id, payment_group_id)
+     VALUES
+     ($1, $2, $3, $4, $5, $6)`,
+    [
+      payment.id,
+      payment.amountcents,
+      new Date(payment.date),
+      payment.description,
+      userId,
+      1
+    ]
+  )
+}
+
 const importPaymentTypes = async () => {
   const types = JSON.parse(fs.readFileSync(typesFile))
   const paymentTypePairs = R.toPairs(types)
@@ -41,7 +58,19 @@ const importPaymentTypes = async () => {
   return R.fromPairs(mappedPaymentTypes)
 }
 
-const importPayments = async (paymentTypes, userMappings) => {}
+const importPayments = async (paymentTypes, userMappings) => {
+  const payments = JSON.parse(fs.readFileSync(paymentsFile))
+  for (const [uuid, payment] of R.toPairs(payments)) {
+    const userId = userMappings[payment.payerid]
+    assert(userId, "No userid found for payment")
+    const paymentType = paymentTypes[payment.name]
+    assert(paymentType, "No payment type found")
+    console.log("Importing payment:")
+    console.log(payment)
+    await insertPayment(payment, paymentType.id, userId)
+    console.log("Imported!")
+  }
+}
 
 const doImport = async () => {
   console.log("Deleting existing data")
@@ -51,7 +80,6 @@ const doImport = async () => {
   const types = await importPaymentTypes()
   const userMappings = JSON.parse(fs.readFileSync(userMappingFile))
   await importPayments(types, userMappings)
-  console.log("payment types", types)
 }
 
 doImport()
