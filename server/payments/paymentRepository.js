0400448b8f0175b33ed5533e4afc7f3406549173
Perttu Auramo <perttu.auramo@gmail.com>
2019-05-28 22:30:18 +0300

    Fix payment user
    Separate it from the current user/group link

--- server/payments/paymentRepository.js
+++ server/payments/paymentRepository.js
@@ -3,14 +3,15 @@ const db = require('../db/db')
 
 const getPayments = async userId => {
   const rawPayments = await db.query(
-    `SELECT p.amount_cents, p.payment_date, ua.name as payer_name, 
+    `SELECT p.amount_cents, p.payment_date, pua.name as payer_name, 
       pt.description as payment_type, p.description 
      FROM payment p
      JOIN payment_group pg ON p.payment_group_id = pg.id
      JOIN payment_group_user pgu ON pg.id = pgu.payment_group_id
-     JOIN user_account ua ON pgu.user_account_id = ua.id
+     JOIN user_account gua ON pgu.user_account_id = gua.id
+     JOIN user_account pua ON p.user_account_id = pua.id
      JOIN payment_type pt ON p.payment_type_id = pt.id
-     WHERE ua.id = $1
+     WHERE gua.id = $1
      ORDER BY p.payment_date desc`,
     [userId]
   )
