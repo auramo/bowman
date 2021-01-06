6441d54fcf90e686408053ba2aedc048b200a2c9
Perttu Auramo <perttu.auramo@gmail.com>
2021-01-06 09:40:57 +0200

    Add sanity checks to date parsing

--- common/date.js
+++ common/date.js
@@ -2,7 +2,14 @@ const parseDate = candidate => {
   if (candidate === null) return null
   const parts = candidate.match(/^(\d{1,2})\.(\d{1,2})\.(\d{4})$/)
   if (parts === null) return null
-  return new Date(parts[3], parts[2] - 1, parts[1])
+  const year = parts[3]
+  const month = parts[2]
+  const day = parts[1]
+  // Just some rough, stupid sanity checks:
+  if (day < 1 || day > 31) return null
+  if (month < 1 || month > 12) return null
+  if (year < 1970 || year > 2100) return null
+  return new Date(day, month - 1, year)
 }
 
 module.exports = { parseDate }
