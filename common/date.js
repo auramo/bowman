692c3a8c4a415f05bca6c7c69561e7a53dfcc882
Perttu Auramo <perttu.auramo@gmail.com>
2021-01-08 22:36:13 +0200

    Fix date parsing

--- common/date.js
+++ common/date.js
@@ -9,7 +9,7 @@ const parseDate = candidate => {
   if (day < 1 || day > 31) return null
   if (month < 1 || month > 12) return null
   if (year < 1970 || year > 2100) return null
-  return new Date(day, month - 1, year)
+  return new Date(year, month - 1, day)
 }
 
 module.exports = { parseDate }
