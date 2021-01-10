c785a23f9535d656bef26d7ee74685cd5a03e40e
Perttu Auramo <perttu.auramo@gmail.com>
2021-01-10 17:20:54 +0200

    Add removing images and containers each time to save limited space

--- pull-and-restart-on-remote.sh
+++ pull-and-restart-on-remote.sh
@@ -4,8 +4,17 @@ set -e
 
 source .env
 
+echo "Removing old container and images"
+
+ssh -i "$SSH_REMOTE_KEY" "$SSH_MACHINE" "( cd bowman && sudo /usr/local/bin/docker-compose stop )"
+
+set +e # Ignore exit codes for these:
+ssh -i "$SSH_REMOTE_KEY" "$SSH_MACHINE" "sudo docker rm bowman_web_1"
+ssh -i "$SSH_REMOTE_KEY" "$SSH_MACHINE" "sudo docker images -a | grep auramo2 | awk '{print $3}' | xargs sudo docker rmi"
+set -e
+
 echo "Pulling and restarting on remote"
 
-ssh -i "$SSH_REMOTE_KEY" "$SSH_MACHINE" "( cd bowman && sudo /usr/local/bin/docker-compose pull && sudo /usr/local/bin/docker-compose up -d )"
+ssh -i "$SSH_REMOTE_KEY" "$SSH_MACHINE" "( cd bowman && sudo /usr/local/bin/docker-compose up -d )"
 
 echo "Done!"
